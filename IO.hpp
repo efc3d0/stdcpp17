3c740e1ade4e0c1f5c0d4df950406766f956f1ae
efc3d0 <pavlikaleksandrblog@gmail.com>
2024-11-05 18:37:36 +0100

    Refactor IO.hpp

--- IO.hpp
+++ IO.hpp
@@ -1,26 +1,67 @@
 #ifndef __IO_H
 #define __IO_H
 
-#include <unistd.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 namespace m34 {
-  template <class T>
-  class InterfaceIO {
-  protected:
-    virtual ssize_t readIO(T buffer) = 0;
-    virtual ssize_t writeIO(T buffer) = 0;
-  };
-
-  template<class T>
-  class RWIo : protected InterfaceIO<T> {
-  public:
-    ssize_t readIO(T buffer) override {
-      return read (0, buffer, sizeof(buffer));
+int strlen(const char *buffer) noexcept {
+    int i = 0;
+    while (*buffer) {
+        buffer++;
+        i++;
     }
-    ssize_t writeIO(T buffer) override {
-      return write (1, buffer, sizeof(buffer));
-    }
-  };
+    return i;
 }
+
+template <class T> class RWIo {
+
+    int writefd;
+    int readfd;
+
+  public:
+    RWIo(int rfd, int wrd) : writefd(wrd), readfd(rfd) {}
+
+    RWIo() : writefd(1), readfd(0) {}
+
+    ssize_t readIO(T buffer) noexcept {
+        return read(readfd, reinterpret_cast<void *>(buffer), sizeof(buffer));
+    }
+    template <class U = T> ssize_t writeIO(U value) noexcept {
+        char buffer[255];
+        int length = convertIntToString(value, buffer);
+        return write(writefd, buffer, length);
+    }
+
+    ssize_t writeIO(const char *buffer) noexcept {
+        return write(writefd, buffer, strlen(buffer));
+    }
+
+    ssize_t writeIO(char value) noexcept { return write(writefd, &value, 1); }
+
+  private:
+    int convertIntToString(int num, char *buffer) {
+        if (num == 0) {
+            buffer[0] = '0';
+            return 1;
+        }
+
+        int index = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            temp /= 10;
+            index++;
+        }
+
+        for (int i = index - 1; i >= 0; --i) {
+            buffer[i] = '0' + (num % 10);
+            num /= 10;
+        }
+
+        return index;
+    }
+};
+}
+
 #endif
